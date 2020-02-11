ba4803f5c83fda5d1d7521e6c36970bbdf3fb556
Natasha <natasha@aMac.fritz.box>
2020-02-11 13:02:03 +0100

    cleaned variable+method names, created stock_object, restructured how to save output

---
+++ classes.py
@@ -0,0 +1,7 @@
+class stock_object:
+  def __init__(self, file_name, stock_name, test_set, train_set):
+    self.file_name = file_name
+    self.stock_name = stock_name
+    self.test_set = test_set
+    self.train_set = train_set
+
--- visualize.py
+++ visualize.py
@@ -4,21 +4,18 @@ import numpy as np
 import argparse
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
+from pathlib import Path
+from classes import *
 
 
-
-
-
-def diagnostic_plots(filename):
+def make_test_train_datasets(file_name):
   #Check metadata of given stock
-  stock_name = extract_stock_name(filename)
   print('-------------------------------------------------------')
-  print('STOCK: {}'.format(stock_name))
-  print('DATA FROM: {}'.format(filename))
-  formatted_data = get_data(filename)
+  print('DATA FROM: {}'.format(file_name))
+  formatted_data = get_data(file_name)
 
-  #Extract train and test set (testsize is set at the beginning of visualize.py)
-  train_set, test_set = train_test_split(formatted_data, test_size = args.testsize, random_state = 42) 
+  #Extract train and test set
+  train_set, test_set = train_test_split(formatted_data, test_size = args.test_size, random_state = 42) 
 
   if args.verbose > 0:
     formatted_data.info()
@@ -29,60 +26,66 @@ def diagnostic_plots(filename):
     print('Test dataset')
     print(test_set.info)
 
-    return train_set
+    return test_set, train_set
   
-def extract_stock_name(filename):
-  if filename.endswith('.us.txt'):
-    filename=filename[:-7].upper()
-  return filename
+def get_stock_name(file_name):
+  if file_name.endswith('.us.txt'):
+    file_name=file_name[:-7].upper()
+  return file_name
 
-def get_data(filename):
+def get_data(file_name):
   #reformat date from Y-M-D to Y.day/365
-  formatted_data = pd.read_csv(filename) 
+  formatted_data = pd.read_csv(file_name) 
   formatted_data.Date = formatted_data['Date'].str.replace('-','').astype(int)
-  formatted_data['Date'] = formatted_data['Date'].apply(date_to_nth_day)
+  formatted_data['Date'] = formatted_data['Date'].apply(get_day_of_year)
   return formatted_data
 
-def date_to_nth_day(date):
+def get_day_of_year(date):
   date = pd.to_datetime(date, format='%Y%m%d')
-  new_year_day = pd.Timestamp(year=date.year, month=1, day=1)
-  days_in_year = number_of_days_in_year(date.year)
-  modified_date = date.year+(((date - new_year_day).days)/(days_in_year)) 
+  first_day = pd.Timestamp(year=date.year, month=1, day=1)
+  days_in_year = get_number_of_days_in_year(date.year)
+  day_of_year = date.year+(((date - first_day).days)/(days_in_year)) 
   if args.verbose > 1:
-    print('date: {} day of year: {} ndays_year: {}, calculated: {}'.format(date,((date - new_year_day).days), days_in_year, modified_date))
-  return modified_date
+    print('date: {} day of year: {} ndays_year: {}, calculated: {}'.format(date,((date - first_day).days), days_in_year, modified_date))
+  return day_of_year
 
-def number_of_days_in_year(year):
+def get_number_of_days_in_year(year):
   first_day = pd.Timestamp(year,1,1)
   last_day = pd.Timestamp(year,12,31)
   #Add 1 to days_in_year (e.g. 20181231 -->2018.99 and 20190101 --> 2019.00)
   number_of_days = (last_day - first_day).days + 1
   return number_of_days
 
-def make_outputdirs(outputdir, filename):
-  if not os.path.exists(outputdir):
-    stock_name = extract_stock_name(filename)
-    os.makedirs(outputdir + '/' + stock_name)
+def make_output_dir(output_dir):
+  if not os.path.exists(output_dir):
+    os.makedirs(output_dir)
   else:
-    print('Exiting. Outputdir, {}, already exists. Delete and rerun, rename, or run something else ;)')
+    print('Exiting. Outputdir, {}, already exists. Delete, rename, or run something else ;)')
     exit()
 
-def make_plots(dataset):
-  stock_name = extract_stock_name(args.filename)
-  for var in dataset:
-    print(var) 
-    ax = dataset.hist(column = 'High') 
-    fig = ax[0][0].get_figure()
-    fig.savefig(args.outputdir + '/' + stock_name + '/' + var + '.pdf')
+def make_nested_dir(output_dir, nested_dir):
+ Path(output_dir + '/' + nested_dir).mkdir(parents=True, exist_ok=True) 
 
+def make_histograms(stock_object):
+  for var in stock_object.train_set:
+    print(var) 
+    ax = stock_object.train_set.hist(column = var) 
+    fig = ax[0][0].get_figure()
+    fig.savefig(args.output_dir + '/' + stock_object.stock_name + '/' + var + '.pdf')
+
+def make_time_dependent_plots(stock_object):
 if __name__ == '__main__':
   parser = argparse.ArgumentParser(description = 'arg parser for visualize.py')
-  parser.add_argument('-f', '--filename', type = str, help = 'input file')
-  parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'count', default = 0, help = 'Enable verbose output (not default). More vs provide more output.')
-  parser.add_argument('-t', '--testsize', type = float, dest = 'testsize', default = 0.2, help = 'Size of test set')
-  parser.add_argument('-o', '--outputdir', type = str, dest = 'outputdir', default = 'output', help = 'name of outputdir')
+  parser.add_argument('-f', '--file_name', type = str, help = 'input file')
+  parser.add_argument('-v', '--verbose', dest = 'verbose', action = 'count', default = 0, help = 'Enable verbose output (not default). Add more vs for more output.')
+  parser.add_argument('-t', '--test_size', type = float, dest = 'test_size', default = 0.2, help = 'Size of test set')
+  parser.add_argument('-o', '--output_dir', type = str, dest = 'output_dir', default = 'output', help = 'name of output_dir')
   args = parser.parse_args()
 
-  make_outputdirs(args.outputdir, args.filename)
-  train_set = diagnostic_plots(args.filename)
-  make_plots(train_set)
+  make_output_dir(args.output_dir)
+
+  make_nested_dir(args.output_dir, get_stock_name(args.file_name))
+  test_set,train_set = make_test_train_datasets(args.file_name)
+  stock_object = stock_object(args.file_name, get_stock_name(args.file_name), test_set, train_set)
+  make_histograms(stock_object)
+  make_time_dependent_plots(stock_object)
