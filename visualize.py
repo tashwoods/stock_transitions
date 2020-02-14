61b9a0d49573789cbc4155dfb36da513fac38482
Natasha <natasha@aMac.fritz.box>
2020-02-14 21:26:18 +0100

    added first version of stock overlay plots

--- visualize.py
+++ visualize.py
@@ -8,6 +8,7 @@ from pathlib import Path
 from pylab import *
 from pandas.plotting import scatter_matrix
 import seaborn as sns
+from matplotlib.pyplot import cm
 from classes import *
 
 
@@ -37,7 +38,7 @@ def make_test_train_datasets(file_name):
 def get_stock_name(file_name):
   if file_name.endswith('.us.txt'):
     if '/' in file_name:
-      file_name=file_name[file_name.rfind('/'):-7].upper()
+      file_name=file_name[file_name.rfind('/')+1:-7].upper()
     else:
       file_name=file_name[:-7].upper()
   return file_name
@@ -93,11 +94,15 @@ def make_histograms(stock_object):
   plt.close('all')
 
 def make_time_dependent_plots(stock_object):
+  time_plots = list()
   for var in stock_object.train_set.columns:
     if var != args.date_name:
-      stock_object.train_set.plot(x = args.date_name, y = var)
+      ax = time_plots.append(stock_object.train_set.plot(x = args.date_name, y = var))
+      plt.xlabel('Date')
+      plt.ylabel(var)
       plt.savefig(args.output_dir + '/' + stock_object.stock_name + '/' + var + '_time.pdf')
   plt.close('all')
+  return time_plots
   
 
 def make_overlay_plots(stock_object):
@@ -224,6 +229,8 @@ if __name__ == '__main__':
   parser.add_argument('-colmax', '--maximum_color_value', type = int, dest = 'color_max', default = 1, help = 'maximum value of color map used in heatmaps')
   parser.add_argument('-palmin', '--minimum_pal_value', type = int, dest = 'pal_min', default = 20, help = 'minimum palette color value used in heatmaps')
   parser.add_argument('-palmax', '--maximum_pal_value', type = int, dest = 'pal_max', default = 220, help = 'maximum palette color value used in heatmaps')
+  parser.add_argument('-indiv_plots', '--indiv_plots', type = int, dest = 'indiv_plots', default = 0, help = 'set to one to have individual stock plots')
+  parser.add_argument('-overlay_stock_plots', '--overlay_stock_plots', type = int, dest = 'overlay_stock_plots', default = 1, help = 'set to one to have overlay stock plots')
   args = parser.parse_args()
 
   make_output_dir(args.output_dir)
@@ -231,6 +238,8 @@ if __name__ == '__main__':
   input_file = open(args.input_file, "r")
  
   stock_objects_list = list() 
+  stock_objects_names = list() 
+
   for file_name in input_file:
     print(file_name)
     file_name = file_name.rstrip()
@@ -240,11 +249,31 @@ if __name__ == '__main__':
     make_nested_dir(args.output_dir, get_stock_name(file_name))
     test_set,train_set = make_test_train_datasets(file_name)
     stock_object = stock_object_class(file_name, get_stock_name(file_name), test_set, train_set)
-    #make_histograms(stock_object)
+    stock_objects_list.append(stock_object)
+    stock_objects_names.append(get_stock_name(file_name))
+
+
+  if args.overlay_stock_plots == 1:
+    #iterate over stock variables
+    for var in stock_objects_list[0].train_set.columns:
+      color = cm.rainbow(np.linspace(0,1,len(stock_objects_list)))
+      print('n: {}'.format(color))
+      for stock,c in zip(range(len(stock_objects_list)), color):
+        if var != args.date_name:
+          plt.plot('Date', var, data = stock_objects_list[stock].train_set, markerfacecolor = c, linewidth = 0.2, label = stock_objects_list[stock].stock_name)
+          plt.xlabel('Date')
+          plt.ylabel(var)
+      plt.legend()
+      plt.savefig(args.output_dir + '/stock_' + var + '_overlay.pdf')    
+      plt.close('all')
+
+ 
+  if args.indiv_plots == 1:
+    make_histograms(stock_object)
     make_overlay_plots(stock_object)
-    #make_time_dependent_plots(stock_object)
     make_scatter_plots(stock_object)
     make_scatter_heat_plots(stock_object)
     make_correlation_plots(stock_object)
+    make_time_dependent_plots(stock_object)
+
     
-    stock_objects_list.append(stock_object)
