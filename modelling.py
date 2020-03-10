fixed rmse calculation for poly and xgb classifiers

modelling.py
@@ -30,38 +30,42 @@ def xgb_predict(stock_object):
   test_prediction = model.predict(x_test_set)
   prediction_dataframe = get_scaled_prediction_dataframe(x_test_set, test_prediction, stock_object)
 
-  #test_prediction_scaled = pd.DataFrame(test_prediction * stock_object.input_args.col_std + stock_object.input_args.col_mean)
-  #prediction_dataframe = x_test_set
-  #prediction_dataframe[predict_var] = test_prediction_scaled.values
-
   unscaled_train_set = get_unscaled_data(stock_object.train_set, stock_object)
   unscaled_test_set = get_unscaled_data(stock_object.test_set, stock_object)
   unscaled_prediction_set = get_unscaled_data(prediction_dataframe, stock_object)
 
-  weekly_total_error, weekly_error_array, weekly_date_array, weekly_data_array = get_general_errors_dataframes(stock_object, stock_object.test_set, model)
+  weekly_total_error = get_general_errors_dataframes(test_prediction, y_test_set.to_numpy().tolist())
 
-  plt.plot(unscaled_train_set[date_name], unscaled_train_set[predict_var], label = 'Train Set')
-  plt.plot(unscaled_test_set[date_name], unscaled_test_set[predict_var], label = 'Test set')
-  plt.plot(unscaled_prediction_set[date_name], unscaled_prediction_set[predict_var], label = 'XGB Prediction RMSE = {}.format(weekly_total_error)')
+  plt.plot(unscaled_test_set[stock_object.input_args.date_name], unscaled_test_set[stock_object.input_args.predict_var], label = 'Test set')
+  plt.plot(unscaled_train_set[stock_object.input_args.date_name], unscaled_train_set[stock_object.input_args.predict_var], label = 'Train Set')
+  plt.plot(unscaled_prediction_set[stock_object.input_args.date_name], unscaled_prediction_set[stock_object.input_args.predict_var], label = 'XGB Prediction RMSE = {}'.format(weekly_total_error))
+
   plt.legend()
+  plt.xlim(2015,2018)
+  plt.xlabel(stock_object.input_args.date_name)
+  plt.ylabel(stock_object.input_args.predict_var)
+  plt.title(stock_object.input_args.predict_var + ' vs. ' + stock_object.input_args.date_name + ': XGB')
   plt.savefig(stock_object.input_args.output_dir + '/' + stock_object.stock_name + '/' + 'stock_' + stock_object.stock_name + '_xgb_overlay.pdf')    
+  plt.close('all')
 
 def poly_fit(stock_object, n):
   x_train_set, y_train_set, x_test_set, y_test_set = get_x_y_test_train(stock_object)
   model = np.poly1d(np.polyfit(stock_object.train_set[stock_object.input_args.date_name], stock_object.train_set[stock_object.input_args.predict_var], n))
-  weekly_total_error, weekly_error_array, weekly_date_array, weekly_data_array = get_general_errors_dataframes(stock_object, stock_object.test_set, model)
+  test_prediction = model(x_test_set[stock_object.input_args.date_name])
+  weekly_total_error = get_general_errors_dataframes(test_prediction, y_test_set.to_numpy().tolist())
   print('Degree: {} Weekly error: {}'.format(n, weekly_total_error))
 
   unscaled_train_set = get_unscaled_data(stock_object.train_set, stock_object)
   unscaled_test_set = get_unscaled_data(stock_object.test_set, stock_object)
 
-  prediction_dataframe = get_scaled_prediction_dataframe(x_test_set, model(x_test_set[stock_object.input_args.date_name]), stock_object)
+  prediction_dataframe = get_scaled_prediction_dataframe(x_test_set, test_prediction, stock_object)
   unscaled_prediction_set = get_unscaled_data(prediction_dataframe, stock_object)
 
   plt.plot(unscaled_test_set[stock_object.input_args.date_name], unscaled_test_set[stock_object.input_args.predict_var], label = 'Test set')
   plt.plot(unscaled_train_set[stock_object.input_args.date_name], unscaled_train_set[stock_object.input_args.predict_var], label = 'Train Set')
   plt.plot(unscaled_prediction_set[stock_object.input_args.date_name], unscaled_prediction_set[stock_object.input_args.predict_var], label = '{} Degree Fit RMSE = {}'.format(n, weekly_total_error))
   plt.legend()
+  plt.xlim(2015,2018)
 
   plt.xlabel(stock_object.input_args.date_name)
   plt.ylabel(stock_object.input_args.predict_var)
@@ -70,48 +74,11 @@ def poly_fit(stock_object, n):
   plt.savefig(stock_object.input_args.output_dir + '/stock_' + stock_object.stock_name + '_poly'+ str(n) + '_overlay.pdf')    
   plt.close('all')
 
-def get_general_errors_dataframes(stock_object, input_data, model):
-  error_array = []
-  error_square_array = []
-  date_array = []
-  data_array = []
-  print('input data columns')
-  print(input_data.columns)
-  print(input_data)
-  date_column_index = input_data.columns.get_loc(stock_object.input_args.date_name)
-  predict_var_column_index = input_data.columns.get_loc(stock_object.input_args.predict_var)
-  print('date index')
-  print(date_column_index)
-  print('predcit var index')
-  print(predict_var_column_index)
-
- 
-
- ### Natasha you need to fix this so it can calculate errors using the xgb model and polynominal models~~~~~~~~~~~~start here
-  for i in range(len(input_data.index)):
-    try:
-      date = input_data.iloc[i, date_column_index]
-      data = input_data.iloc[i, predict_var_column_index]
-      predic = model(date)
-    except:
-      date = input_data.iloc[i, date_column_index]
-      data = stock_object.test_set.drop(predict_var, axis = 1)
-      predic = model.predict(date)
-
-    date_array.append(date)
-    error_array.append(predic-data)
-    error_square_array.append((predic - data)**2)
-    data_array.append(data)
-
+def get_general_errors_dataframes(model, data):
+  error_array = model - data
+  error_square_array = error_array**2
   total_error = round(math.sqrt((1/len(error_square_array))*sum(error_square_array)),2)
-  print('date')
-  print(date_array)
-  print('new error array')
-  print(error_array)
-  print('total error')
-  print(total_error)
-  
-  return total_error, error_array, date_array, data_array
+  return total_error
 
 def split_dataframe_into_dataframes(dataframe, chunk_size):
   index_marks = range(0,len(dataframe.index), chunk_size)