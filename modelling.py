fixed scaling of variables for XGB Simple Prediction, and iterative scaling for SeqXGB

modelling.py
@@ -19,13 +19,11 @@ def get_x_y_test_train(stock_object, scale, i):
       test_set = stock_object.test_set_unscaled
       if i != 0:
         if i == 1:
-          array_to_add = test_set.iloc[0]
-          df_to_add = pd.DataFrame(array_to_add, index = test_set.index, columns = test_set.columns)
-          train_set = train_set.append(df_to_add)
-          test_set = test_set.iloc[i:]
+          append = test_set.iloc[0]
         else:
-          train_set = train_set.append(test_set.iloc[:i-1])
-          test_set = test_set.iloc[i:]
+          append = test_set.iloc[:i]
+        train_set = train_set.append(append)
+        test_set = test_set.iloc[i:]
 
     train_set_features = scaler.fit_transform(train_set)
     test_set_features = scaler.transform(test_set)
@@ -39,7 +37,7 @@ def get_x_y_test_train(stock_object, scale, i):
   x_test_set = test_set.drop(predict_var, axis = 1)
   y_test_set = test_set[[predict_var]]
 
-  return x_train_set, y_train_set, x_test_set, y_test_set, scaler
+  return test_set, train_set, x_train_set, y_train_set, x_test_set, y_test_set, scaler
 
 def get_unscaled_data(dataset, stock_object, scaler):
   array_data = scaler.inverse_transform(dataset)
@@ -47,21 +45,21 @@ def get_unscaled_data(dataset, stock_object, scaler):
   return formatted_data
 
 def get_prediction_dataframe(x_test_set, test_prediction, stock_object):
-  test_prediction_scaled = pd.DataFrame(test_prediction * stock_object.input_args.col_std + stock_object.input_args.col_mean)
+  test_prediction_scaled = pd.DataFrame(test_prediction)
   prediction_dataframe = x_test_set
   prediction_dataframe[stock_object.input_args.predict_var] = test_prediction_scaled.values
   return prediction_dataframe
 
 def xgb_sequential_predict(stock_object, n_estimators, max_depth, learning_rate, min_child_weight, subsample):
-  x_train_set0, y_train_set0, x_test_set0, y_test_set0, scaler = get_x_y_test_train(stock_object, -1, 0)
+  test_set0, train_set0, x_train_set0, y_train_set0, x_test_set0, y_test_set0, scaler = get_x_y_test_train(stock_object, -1, 0)
   predict_var = stock_object.input_args.predict_var
   test_prediction = []
   test_prediction_unscaled = []
   #iterate through test set
   for i in range(len(stock_object.test_set_unscaled.index)):
     #Iteratively augment, scale and model test set
-    x_train_set, y_train_set, x_test_set, y_test_set, scaler = get_x_y_test_train(stock_object, 1, i)
-    model = XGBRegressor(n_estimators = n_estimators, max_depth = max_depth, learning_rate = learning_rate, min_child_weight = min_child_weight, subsample = subsample, colsample_bytree = stock_object.input_args.col_std, colsample_bylevel = stock_object.input_args.col_mean)
+    test_set, train_set, x_train_set, y_train_set, x_test_set, y_test_set, scaler = get_x_y_test_train(stock_object, 1, i)
+    model = XGBRegressor(n_estimators = n_estimators, max_depth = max_depth, learning_rate = learning_rate, min_child_weight = min_child_weight, subsample = subsample)
     model.fit(x_train_set.values, y_train_set.values)
     #Predict stock price for only first entry in test set, as this is an iterative BDT
     prediction_array = x_test_set.iloc[0]
@@ -77,6 +75,7 @@ def xgb_sequential_predict(stock_object, n_estimators, max_depth, learning_rate,
   test_prediction_unscaled = np.asarray(test_prediction_unscaled)
   unscaled_prediction_set = get_prediction_dataframe(x_test_set0, test_prediction_unscaled, stock_object)
 
+
   #Obtain and Save Modelling Errors
   unscaled_weekly_total_error = get_general_errors_dataframes(test_prediction_unscaled, y_test_set0.to_numpy().tolist())
   print('UnScaled XGB error: {}'.format(unscaled_weekly_total_error))
@@ -89,22 +88,24 @@ def xgb_sequential_predict(stock_object, n_estimators, max_depth, learning_rate,
   
 def xgb_predict(stock_object, n_estimators, max_depth, learning_rate, min_child_weight=1, subsample=1):
   #Scale, split and model data
-  x_train_set, y_train_set, x_test_set, y_test_set, scaler = get_x_y_test_train(stock_object, 0, 0)
-  model = XGBRegressor(n_estimators = n_estimators, max_depth = max_depth, learning_rate = learning_rate, min_child_weight = min_child_weight, subsample = subsample, colsample_bytree = stock_object.input_args.col_std, colsample_bylevel = stock_object.input_args.col_mean)
-  model.fit(x_train_set, y_train_set)
-  test_prediction = model.predict(x_test_set)
+  predict_var = stock_object.input_args.predict_var
+  test_set, train_set, x_train_set, y_train_set, x_test_set, y_test_set, scaler = get_x_y_test_train(stock_object, 0, 0)
+  model = XGBRegressor(n_estimators = n_estimators, max_depth = max_depth, learning_rate = learning_rate, min_child_weight = min_child_weight, subsample = subsample)
+  model.fit(x_train_set.values, y_train_set.values)
+  test_prediction = model.predict(x_test_set.values)
   scaled_prediction_set = get_prediction_dataframe(x_test_set, test_prediction, stock_object)
   unscaled_prediction_set = get_unscaled_data(scaled_prediction_set, stock_object, scaler)
 
   #Print and Save Modelling Errors
   scaled_weekly_total_error = get_general_errors_dataframes(test_prediction, y_test_set.to_numpy().tolist())
-  unscaled_weekly_total_error = get_general_errors_dataframes(unscaled_prediction_set[stock_object.input_args.predict_var], stock_object.test_set_unscaled[stock_object.input_args.predict_var])
+  unscaled_weekly_total_error = get_general_errors_dataframes(unscaled_prediction_set[predict_var], stock_object.test_set_unscaled[predict_var])
   stock_object.add_unscaled_model("XGBUnScaled", unscaled_prediction_set, unscaled_weekly_total_error)
   print('UnScaled XGB error: {}'.format(unscaled_weekly_total_error))
   print('Scaled XGB error: {}'.format(scaled_weekly_total_error))
 
   #Plot Results 
   prediction_overlay_plot(stock_object.test_set_unscaled, stock_object.train_set_unscaled, unscaled_prediction_set, unscaled_weekly_total_error, 'XGBUnScaled', stock_object)
+  prediction_overlay_plot(test_set, train_set, scaled_prediction_set, scaled_weekly_total_error, 'XGBScaled', stock_object)
 
   return mean_squared_error(y_test_set, test_prediction)
 